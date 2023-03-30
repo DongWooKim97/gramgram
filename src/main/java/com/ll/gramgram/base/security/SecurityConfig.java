91c684bb3c325d7b06ea075b09a96148f26761bd
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-30 10:56:43 +0900

    feat: setup spring security

---
+++ src/main/java/com/ll/gramgram/base/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.ll.gramgram.base.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    // 내가 하는 로그인폼 커스텀(직접 사용할 url 명시)
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin(formLogin -> formLogin
+                        .loginPage("/member/login"))
+                .logout(logout -> logout
+                        .logoutUrl("/member/logout"));
+
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
