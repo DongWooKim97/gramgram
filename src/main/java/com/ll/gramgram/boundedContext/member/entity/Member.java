32e6d252f98c2b1ff0808f7ca60405cceb25e02b
김동우 <nanaukim@naver.com>
2023-04-03 13:27:14 +0900

    feat: grant admin user

--- src/main/java/com/ll/gramgram/boundedContext/member/entity/Member.java
+++ src/main/java/com/ll/gramgram/boundedContext/member/entity/Member.java
@@ -6,10 +6,10 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -25,20 +25,22 @@ public class Member {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
-
     @CreatedDate
     private LocalDateTime createDate;
-
     @LastModifiedDate
     private LocalDateTime modifyDate;
-
     @Column(unique = true)
     private String username;
-    
     private String password;
 
     public List<? extends GrantedAuthority> getGrantedAuthorities() {
         List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+        grantedAuthorities.add(new SimpleGrantedAuthority("member"));
+
+        if (username.equals("admin")) {
+            grantedAuthorities.add(new SimpleGrantedAuthority("admin"));
+        }
 
         return grantedAuthorities;
     }
